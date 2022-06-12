182893a4cfade6e2fbad5c3bf9d4b62bc4f6b0f9
Typejunjie <2282756118@qq.com>
2022-06-12 11:35:55 +0800

    inital project

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './plugins/axios'
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+
+createApp(App).use(store).use(router).mount('#app')
